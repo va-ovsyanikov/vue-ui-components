b654620276dd6fcfd7be8401f5b34136af3ae96f
va-ovsyanikov <va.ovsyanikov@gmail.com>
2022-10-27 13:52:20 +0300

    add dockerfile

--- vite.config.js
+++ vite.config.js
@@ -4,33 +4,42 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'dme-web-ui',
-      fileName: (format) => `dme-web-ui.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'vue'
-        }
-      }
-    }
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-    },
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-
+	build: {
+		//для npm пакета  ----
+		// lib: {
+		// 	entry: path.resolve(__dirname, 'src/index.js'),
+		// 	name: 'dme-web-ui',
+		// 	fileName: (format) => `dme-web-ui.${format}.js`,
+		// },
+		// rollupOptions: {
+		// 	external: ['vue'],
+		// 	output: {
+		// 		globals: {
+		// 			vue: 'vue'
+		// 		}
+		// 	}
+		// }
+		//---------------------------
+	},
+	publicDir:'./src/fonts', //добавляет файлы в папку dist  при сборке
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		coverage: {
+			reporter: ['text', 'json', 'html'],
+		},
+	},
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	// для docker vite
+	// server: {
+	// 	host: '0.0.0.0',
+	// 	watch: {
+	// 		usePolling: true
+	// 	}
+	// }
 })
